e88232d755cc5a22032c60b1ba7a800d1f8ea698
dijass18 <muhdijas18@gmail.com>
2024-03-04 14:11:58 +0700

    String

---
+++ dart_project/bin/latihan1/Pertemuan1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  // 01
+  String data = "Muh Sajid Abdillah";
+  print(data);
+
+  // 02
+  print("Masukkan nama:");
+  String? nama = stdin.readLineSync();
+  print("Nama anda adalah ${nama}");
+
+  // 03
+}
