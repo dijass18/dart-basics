8b03388a752ce9adbea8e4694a5d2a9f30e34bd6
dijass18 <muhdijas18@gmail.com>
2024-03-04 14:51:36 +0700

    Latihan 1 Soal 4

---
+++ dart_project/bin/latihan1/04.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan p :');
+  int? p = int.parse(stdin.readLineSync()!);
+  stdout.write('Masukkan t :');
+  int? t = int.parse(stdin.readLineSync()!);
+  stdout.write('Masukkan r :');
+  int? r = int.parse(stdin.readLineSync()!);
+  double hasil = (p * t * r) / 100;
+
+  print("Hasilnya adala ${hasil}");
+}
